94a942803d72f0b0598b9be21271dffc55b7e072
RealDiviner <rhysmontano64@gmail.com>
2026-06-24 18:22:30 -0700

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,13 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+// 1. Import your new submission page file
+import Submit from './pages/Submit.js'; 
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    // 2. Register the path route rule handler
+    { path: '/submit', component: Submit }, 
 ];
